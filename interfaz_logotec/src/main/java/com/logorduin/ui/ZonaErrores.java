232d7456a3aa31f43a4865805075b93a1be4738d
jriveramr <j.rivera@estudiantec.cr>
2025-09-28 00:04:40 -0600

    implementacion interfaz IDE basica

---
+++ interfaz_logotec/src/main/java/com/logorduin/ui/ZonaErrores.java
@@ -0,0 +1,31 @@
+package com.logorduin.ui;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
+
+public class ZonaErrores extends VBox {
+
+    private final ListView<String> listaErrores;
+
+    public ZonaErrores() {
+        Label titulo = new Label("Errores");
+        listaErrores = new ListView<>();
+        listaErrores.setPrefHeight(100);
+        this.getChildren().addAll(titulo, listaErrores);
+    }
+
+    public void mostrarErrores(List<String> errores) {
+        listaErrores.getItems().setAll(errores);
+    }
+
+    public void limpiar() {
+        listaErrores.getItems().clear();
+    }
+
+    public ListView<String> getListView() {
+        return listaErrores;
+    }
+}
